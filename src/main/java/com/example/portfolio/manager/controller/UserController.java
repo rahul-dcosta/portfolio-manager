694977f6cd1be7b3fb6fef1488b98ee46284ba75
Rahul D'Costa <rdcosta@umich.edu>
2024-07-31 00:34:41 -0400

    added logging for create user

--- src/main/java/com/example/portfolio/manager/controller/UserController.java
+++ src/main/java/com/example/portfolio/manager/controller/UserController.java
@@ -5,6 +5,9 @@ import com.example.portfolio.manager.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @RestController
 @RequestMapping("/api/users")
@@ -16,6 +19,7 @@ public class UserController {
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user) {
         User newUser = userService.saveUser(user);
+        logger.info("New user created: {}", user.getUsername()); // Add this line
         return ResponseEntity.ok(newUser);
     }
 }
